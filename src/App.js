fe330dd574c06322172741faa12d676af1bf5120
chanseoky <chanseoky@naver.com>
2020-04-13 02:26:37 +0900

    fdf

--- src/App.js
+++ src/App.js
@@ -1,62 +1,89 @@
-import React, { useCallback, useState,useRef  } from 'react'
+import React, { useCallback, useState,useRef, useReducer  } from 'react'
 
 import './App.css';
 import TodoTemplate from './components/TodoTemplate'
 import TodoInsert from './components/TodoInsert'
 import TodoList from './components/TodoList'
 
+function createBulkTodos () {
+  const array = [];
+  for(let i =1; i<=2500; i++){
+    array.push({
+      id:i,
+      text:`할 일 ${i}`,
+      checked: false,
+    });
+  }
+  return array;
+}
+
+function todoReducer (todos, action) {
+  switch (action.type) {
+    case 'INSERT':
+      // {type:'INSERT', todo : {id:1, text:'todo', checked:false}}
+      return todos.concat(action.todo)
+    
+    case 'REMOVE':
+      // {type: 'REMOVE', id:1}
+      return todos.filter(todo => todo.id !== action.id)
+    
+    case 'TOGGLE':
+      // {type:'REMOVE', id:1}
+      return todos.map(todo =>
+        todo.id === action.id ? { ...todo, checked: !todo.checked } : todo,
+      )
+    default :
+      return todos
+  }
+}
+
+
+
+
 function App() {
-  const [todos, setTodos] = useState([
-    {id: 1,
-    text: '리액트의 기초 알아보기',
-    checked: false,},
-    {id: 2,
-      text: '컴포넌트 스타일링 해보기',
-      checked: true,},
-    {id: 3,
-      text: '일정 관리 앱 만들어 보기',
-      checked:false,},
-  ]);
+
+  const [todos, dispatch] = useReducer(todoReducer, undefined, createBulkTodos);
+  // const [todos, setTodos] = useState(createBulkTodos);
+ 
   
   //고유값으로 사용될 id
-  // ref를 사용하ㅕㅇ 변수담기
-  const nextId = useRef(4);
+  // ref를 사용하du 변수담기
+  const nextId = useRef(2501);
+  // const nextId = useRef(4);
   
-  const onInsert = useCallback(
-    text => {
-      
+  
+  
+  
+  //setTodos를 사용 할때 그안에 todos -> 넣어주면 성능향상
+  const onInsert = useCallback(text => {
+
       const todo = {
         id: nextId.current,
         text,
         checked:false,
       };
-      
-      setTodos(todos.concat(todo));
+
+      // setTodos(todos => todos.concat(todo));  이거대신
+    dispatch({type: 'INSERT', todo});
       nextId.current += 1; //nextId +1 씩
-    },
-    [todos],
-  );
-  
-  
+    },[]);
+
+
   const onRemove = useCallback(
     id => {
-      setTodos(todos.filter(todo =>
-        todo.id !==id));
-    },
-    [todos],
-  )
-  
+      // setTodos(todos => todos.filter(todo => todo.id !==id));
+      dispatch({type:'REMOVE', id});
+    },[])
+
   const onToggle = useCallback(
     id => {
-      setTodos(
-        todos.map(todo =>
-      todo.id === id ? {...todo, checked: !todo.checked} : todo,
-        )
-      );
-    },
-    [todos],
-  )
-  
+      // setTodos( todos => todos.map(todo => todo.id === id ? {...todo, checked: !todo.checked} : todo,)
+      dispatch({type:'TOGGLE', id});
+    },[])
+
+
+
+
   return (
     <TodoTemplate>
       <TodoInsert onInsert={onInsert}/>
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,21 +1,25 @@
-import React from 'react';
+import React, { useCallback } from 'react'
 import './TodoList.scss'
 import TodoListItem from './TodoListItem'
+import {List} from 'react-virtualized'
 
 //todos배열을 props로 받아온후, 배열 map을 사용해 여러개의 컴포넌트로 변환하여 보여줌.
 const TodoList =({todos,onRemove,onToggle}) =>{
+  const rowRenderer = useCallback(
+    ({index,key,style}) => {
+      const todo = todos[index];
+      
   return (
-    <div className="TodoList">
-      {todos.map(todo => (
-        <TodoListItem
+    <TodoListItem
           todo={todo}
           key={todo.id}
           onRemove={onRemove}
           onToggle={onToggle}
+        
         />
         ))}
     </div>
   )
 }
 
-export default TodoList;
+export default React.memo(TodoList);
