217da3abc06d975c095924176c988a1dc7bdc5ff
chanseoky <chanseoky@naver.com>
2020-04-14 02:10:14 +0900

    fdf

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -5,6 +5,8 @@ import {List} from 'react-virtualized'
 
 //todos배열을 props로 받아온후, 배열 map을 사용해 여러개의 컴포넌트로 변환하여 보여줌.
 const TodoList =({todos,onRemove,onToggle}) =>{
+  
+  //List 컴포넌트 사용하기 위해. List 컴포넌트에서 각 todoitem을 렌더링 할때 사용. list 컴포넌트의 props로 설정.
   const rowRenderer = useCallback(
     ({index,key,style}) => {
       const todo = todos[index];
@@ -12,13 +14,26 @@ const TodoList =({todos,onRemove,onToggle}) =>{
   return (
     <TodoListItem
           todo={todo}
-          key={todo.id}
+          key={key}
           onRemove={onRemove}
           onToggle={onToggle}
-        
+          style={style}
         />
-        ))}
-    </div>
+        );},
+    [onRemove,onToggle,todos],
+  );
+  
+  return (
+    <List
+      className="TodoList"
+      width={512}
+      height={513}
+      rowCount={todos.length}
+      rowHeight={57}
+      rowRenderer={rowRenderer}
+      list={todos}
+      style={{outline: 'none'}}
+      />
   )
 }
 
--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -10,14 +10,18 @@ import cn from 'classnames';
 //각 할일 항목에 대한 정보를 보여주는 컴포넌트, todo 객체를 props로 받아와서 상태에 따라 다른 스타일의 ui를 보여준다.
 
 
-const TodoListItem =({todo,onRemove,onToggle}) => {
+const TodoListItem =({todo,onRemove,onToggle,style}) => {
   const {id,text,checked} = todo;
   
   return (
+    
+    <div className="TodoListItem-virtualized" style={style}>
     <div className="TodoListItem">
-      
-      
-      <div className={cn('checkbox',{checked})} onClick={() => onToggle(id)}>
+      <div
+        className={cn('checkbox',{checked})}
+        onClick={() => onToggle(id)}
+      >
+        
         {checked ? <MdCheckBox/> : <MdCheckBoxOutlineBlank />}
         <div className="text">{text}</div>
       </div>
@@ -27,7 +31,10 @@ const TodoListItem =({todo,onRemove,onToggle}) => {
         <MdRemoveCircleOutline />
       </div>
     </div>
+    </div>
   )
 }
 
-export default React.memo(TodoListItem);
+export default React.memo(
+  TodoListItem,
+  (prevProps, nextProps) => prevProps.todo=== nextProps.todo,);
